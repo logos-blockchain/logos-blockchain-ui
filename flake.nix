f4c4b090d4c809baa9921ac80a6f590ea12e5696
Khushboo Mehta <mehtakhushboo@hotmail.com>
2026-06-26 22:25:45 +0200

    chore: bump flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Blockchain UI plugin for the Logos application";
 
   inputs = {
-    logos-module-builder.url = "github:logos-co/logos-module-builder/38ddf92c1f240f4e420d300a1fbabb1609d5db01";
+    logos-module-builder.url = "github:logos-co/logos-module-builder/92ef691ea72844134f6c68fb447d37f855fc9690";
     nix-bundle-lgx.url = "github:logos-co/nix-bundle-lgx";
     # v0.0.3
     blockchain_module.url = "github:logos-blockchain/logos-blockchain-module?ref=0.1.3-rc.13";
