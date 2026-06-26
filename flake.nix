fd2dedfebf9739288dfb68669696a6bc8e1d2b08
Daniel <sanchez.quiros.daniel@gmail.com>
2026-06-26 23:40:11 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     logos-module-builder.url = "github:logos-co/logos-module-builder/38ddf92c1f240f4e420d300a1fbabb1609d5db01";
     nix-bundle-lgx.url = "github:logos-co/nix-bundle-lgx";
     # v0.0.3
-    blockchain_module.url = "github:logos-blockchain/logos-blockchain-module?ref=0.2.0";
+    blockchain_module.url = "github:logos-blockchain/logos-blockchain-module?ref=0.2.0-rc.1";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
